aeaf595ae94fea9ed2d95ca26efd3117bbec142d
Samuel44-hue <samueljoseph2444@gmail.com>
2024-11-18 10:52:39 +0530

    Update models.py

--- App_1/models.py
+++ App_1/models.py
@@ -1,30 +1,27 @@
 from django.db import models
-import os
+from cloudinary.models import CloudinaryField
 
 class Product_list(models.Model):
-    Product_Image = models.ImageField(null=True)
-    Product_Name = models.CharField(max_length=250,null=True)
-    Product_Cat = models.CharField(max_length=250,null=True)
+    Product_Image = CloudinaryField('image', null=True)  # Cloudinary storage for the image
+    Product_Name = models.CharField(max_length=250, null=True)
+    Product_Cat = models.CharField(max_length=250, null=True)
     Product_Description = models.JSONField(blank=True, default=list)
 
     def __str__(self):
         return '{}'.format(self.Product_Name)
-    
+
+    # No need to manually delete files because Cloudinary handles file management.
     def delete(self, *args, **kwargs):
-        if self.Product_Image:
-            if os.path.isfile(self.Product_Image.path):
-                os.remove(self.Product_Image.path)
-                super().delete(*args, **kwargs)
+        super().delete(*args, **kwargs)  # Call the base class delete method
+
 
 class Project(models.Model):
-    Project_Image = models.ImageField(null=True)
-    Project_Name = models.CharField(max_length=250,null=True)
+    Project_Image = CloudinaryField('image', null=True)  # Cloudinary storage for the image
+    Project_Name = models.CharField(max_length=250, null=True)
 
     def __str__(self):
         return '{}'.format(self.Project_Name)
-    
+
+    # No need to manually delete files because Cloudinary handles file management.
     def delete(self, *args, **kwargs):
-        if self.Project_Image:
-            if os.path.isfile(self.Project_Image.path):
-                os.remove(self.Project_Image.path)
-                super().delete(*args, **kwargs)
+        super().delete(*args, **kwargs)  # Call the base class delete method
